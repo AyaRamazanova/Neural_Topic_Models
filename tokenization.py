1ce3fd04b6a4d0d018ba1b4e7602781743ad5460
AyaRamazanova <ayazhan1508@gmail.com>
2021-01-27 20:16:08 +0300

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -8,6 +8,7 @@ from pyhanlp import *
 #     tokens = [t.word for t in HanLP.segment(sent)]
 #     tokens = [re.sub(pat, r'', t).strip() for t in tokens]
 #     tokens = [t for t in tokens if t != '']
+#
 #     if stopwords is not None:
 #         tokens = [t for t in tokens if not (t in stopwords)]
 #     return tokens
@@ -15,26 +16,24 @@ from pyhanlp import *
 def Tokenizer(sent, stopwords=None):
     tokens = sent.split()
     del tokens[0]
-    tokens = [t for t in tokens if t != '']
-    tokens = list(filter(lambda token: token != '', tokens))
+    #tokens = list(filter(lambda token: token != '', tokens))
+    tokens = list(filter(lambda token: len(tokens) > 3, tokens))
     if stopwords is not None:
         tokens = [t for t in tokens if not (t in stopwords)]
     return tokens
 
 
-'''
-def Tokenizer(sent,stopwords=None):
-    # Tokenizer for English. 
-    pat = re.compile(r'[0-9!"#$%&\'()*+,-./:;<=>?@—，。：★、￥…【】（）《》？“”‘’！\[\\\]^_`{|}~\u3000]+')
-    tokens = [re.sub(pat,r'',t).strip() for t in sent.split(' ')]
-    tokens = [t for t in tokens if t!='']
-    from nltk.stem import WordNetLemmatizer
-    wnl = WordNetLemmatizer()
-    tokens = [wnl.lemmatize(t).lower() for t in tokens]    
-    if stopwords is not None:
-        tokens = [t for t in tokens if not (t in stopwords)]                                               
-    return tokens
-'''
+# def Tokenizer(sent,stopwords=None):
+#     # Tokenizer for English.
+#     pat = re.compile(r'[0-9!"#$%&\'()*+,-./:;<=>?@—，。：★、￥…【】（）《》？“”‘’！\[\\\]^_`{|}~\u3000]+')
+#     tokens = [re.sub(pat,r'',t).strip() for t in sent.split(' ')]
+#     tokens = [t for t in tokens if t != '']
+#     from nltk.stem import WordNetLemmatizer
+#     wnl = WordNetLemmatizer()
+#     tokens = [wnl.lemmatize(t).lower() for t in tokens]
+#     if stopwords is not None:
+#         tokens = [t for t in tokens if not (t in stopwords)]
+#     return tokens
 
 if __name__ == '__main__':
     print(Tokenizer('他拿的是《红楼梦》？！我还以为他是个Foreigner———'))
